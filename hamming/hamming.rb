0a8aa4bf228548fb59fe4cb1b1368c54abd35b8d
Amanda Ross <amikaross@gmail.com>
2023-05-10 11:37:25 -0600

    refactor to use inject

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,12 +1,9 @@
 class Hamming
   def self.compute(strand_1, strand_2)
-    if strand_1.length != strand_2.length
-      raise ArgumentError.new
+    raise ArgumentError.new if strand_1.length != strand_2.length
+
+    (0..strand_1.length - 1).inject(0) do |diff, i|
+      strand_1[i] != strand_2[i] ? diff + 1 : diff + 0
     end
-    differences = 0
-    (0..strand_1.length - 1).each do |i|
-      differences += 1 if strand_1[i] != strand_2[i]
-    end
-    differences
   end
 end
