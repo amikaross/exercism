86694dbe5b161d83c543c6043d0d24760434bd9a
Amanda Ross <amikaross@gmail.com>
2023-06-23 12:16:24 -0600

    First complete iteration - all tests passing

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -10,11 +10,54 @@
 # - Neptune: orbital period 164.79132 Earth years
 
 class SpaceAge
+  ORBITS = {
+    "earth" => 1.0,
+    "mercury" => 0.2408467,
+    "venus" => 0.61519726,
+    "mars" => 1.8808158,
+    "jupiter" => 11.862615,
+    "saturn" => 29.447498,
+    "uranus" => 84.016846,
+    "neptune" => 164.79132
+  }
+
   def initialize(seconds)
-    @seconds = seconds.to_f
+    @seconds = seconds
   end
 
   def on_earth
-    result = (@seconds / 31557600).round(2)
+    calculate("earth")
+  end
+
+  def on_mercury
+    calculate("mercury")
+  end
+
+  def on_venus
+    calculate("venus")
+  end
+
+  def on_mars
+    calculate("mars")
+  end
+
+  def on_jupiter
+    calculate("jupiter")
+  end
+
+  def on_saturn
+    calculate("saturn")
+  end
+
+  def on_uranus
+    calculate("uranus")
+  end
+
+  def on_neptune
+    calculate("neptune")
+  end
+
+  def calculate(planet)
+    (@seconds / (31557600*ORBITS[planet])).round(2)
   end
 end
