0fd7156e2a08a795992d847f4beb0afc2a7eed7f
Amanda Ross <amikaross@gmail.com>
2023-06-24 11:35:56 -0600

    difference of squares first iteration - all tests passing

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,7 +1,20 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+  def initialize(length)
+    @collection = (1..length).to_a
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+  def sum_of_squares
+    @collection.inject(0) do |sum, val| 
+      sum + (val*val)
+    end
+  end
+
+  def square_of_sum
+    sum = @collection.sum
+    sum * sum 
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
