de8dd39c76da2fa1325c1a500a04a2811f0a449e
Amanda Ross <amikaross@gmail.com>
2023-06-27 14:07:23 -0600

    Second iteration - anagram

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,18 +1,13 @@
 class Anagram
   def initialize(word)
     @word = word.downcase
-    word_chars = word.downcase.chars
-    @word_map = word_chars.each_with_object({}) { |char, map| map[char] ? map[char] += 1 : map[char] = 1 }
+    @word_chars = @word.chars.sort
   end
 
-  def match(word_list)
-    word_list.each_with_object([]) do |word, anagrams|
-      anagrams << word if is_anagram?(word.downcase.chars) && @word != word.downcase
+  def match(options)
+    options.select do |option|
+      option_chars = option.downcase.chars.sort
+      option_chars == @word_chars unless option.downcase.eql?(@word)
     end
-  end
-
-  def is_anagram?(word_chars)
-    anagram_map = word_chars.each_with_object({}) { |char, map| map[char] ? map[char] += 1 : map[char] = 1 }
-    anagram_map == @word_map
   end
 end
