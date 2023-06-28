73dcb0b7cd1d47d4218988b87d5a745b60acde98
Amanda Ross <amikaross@gmail.com>
2023-06-28 10:50:16 -0600

    Second iteration with custom transpose method

--- transpose/transpose.rb
+++ transpose/transpose.rb
@@ -6,8 +6,21 @@ module Transpose
     padded_rows = rows.map { |row| row.ljust(largest_row) }
 
     padded_rows.map(&:chars)
-               .transpose
+               .custom_transpose
                .map { |new_row| new_row.join.rstrip.gsub("_", " ") }
                .join("\n")
+  end
+end
+
+# could have used array method transpose on line 9, 
+# but I wanted to try to implement it myself as well
+
+class Array
+  def custom_transpose
+    self.each_with_object([]) do |row, output|
+      row.each_with_index do |char, j|         
+        output[j] ? output[j] << char : output << [char]
+      end
+    end
   end
 end
