efb0e78dd60566c17da4c8e03aa13522bfbebba7
Amanda Ross <amikaross@gmail.com>
2023-06-28 11:56:59 -0600

    Second iteration without using flatten built-in for flatten array

--- flatten-array/flatten_array.rb
+++ flatten-array/flatten_array.rb
@@ -1,5 +1,12 @@
 class FlattenArray
   def self.flatten(input)
-    input.flatten.reject { |e| e == nil }
+    input.each_with_object([]) do |element, result|
+      next if element.nil?
+      if element.class == Array
+        result.concat(flatten(element))
+      else
+        result.append(element)
+      end
+    end
   end
 end
