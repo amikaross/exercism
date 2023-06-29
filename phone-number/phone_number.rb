1fdc65296ec3d739381b780ae977eb9fb8e70f0b
Amanda Ross <amikaross@gmail.com>
2023-06-29 10:40:25 -0600

    second iteration phone_number

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,20 +1,10 @@
 module PhoneNumber
-  NUMBERS = ("0".."9").to_a
-
   def self.clean(dirty_number)
-    result = dirty_number.chars.select do |char|
-      NUMBERS.include?(char)
-    end.join
-
-    result = result[1..-1] if result[0] == "1" && result.length == 11
-
-    valid_number?(result) ? result : nil
-  end
-
-  def self.valid_number?(result)
-    return false if result.length != 10
-    return false if result[0] == "0" || result[0] == "1"
-    return false if result[3] == "0" || result[3] =="1"
-    true
+    result = dirty_number.gsub(/[^0-9]/, "").delete_prefix("1")
+    if result.match(/\A(\d{3})?[01]/) || result.length != 10
+      nil
+    else 
+      result
+    end
   end
 end
