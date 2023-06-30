2d49abc8ad39d9fb571c3261c90f222d7076cb27
Amanda Ross <amikaross@gmail.com>
2023-06-30 14:22:37 -0600

    Multiple assignment - etl

--- etl/etl.rb
+++ etl/etl.rb
@@ -1,7 +1,6 @@
 module ETL
   def self.transform(old)
-    old.each_with_object({}) do |old, new_hash|
-      value, letter_array = old
+    old.each_with_object({}) do |(value, letter_array), new_hash|
       letter_array.each { |letter| new_hash[letter.downcase] = value }
     end
   end
