b2c31e4a912f7f1c59789d47b6ce5d5202b26bd2
Amanda Ross <amikaross@gmail.com>
2023-07-31 08:03:20 -0600

    Setup for pythagorean triplet

---
+++ pythagorean-triplet/pythagorean_triplet.rb
@@ -0,0 +1,14 @@
+module PythagoreanTriplet
+  def self.triplets_with_sum(num)
+    triplets = []
+    sum = num
+    (1..num / 3).each do |a|
+      require 'pry'; binding.pry
+    end
+  end
+end
+
+# a² + b² = c²
+# a < b < c
+# a + b + c = N
+
